2f2cf67caa2720e4cf6ae2ad3d5f74b19029ba45
gleitons <31367377+gleitons@users.noreply.github.com>
2021-08-25 07:27:57 -0300

    sibutramin

--- includes/form_18394.php
+++ includes/form_18394.php
@@ -1,17 +1,17 @@
 <?php	
 	
 	
-	$input_2431 = $_POST['input_2431'];
-	$input_2985 = $_POST['input_2985'];
-	$input_606 = $_POST['input_606'];
-	$optin = $_POST['optin'];
+	$tentando_engravidar = $_POST['tentando_engravidar'];
+	$ = $_POST[''];
+	$ = $_POST[''];
+	$optin_18394 = $_POST['optin_18394'];
 	
 	$to = 'naturfiveoficial@gmail.com'; // Email submissions are sent to this email
 
 	// Create email	
 	$email_subject = "Mam Caps Propaganda";
 	$email_body = "Mensagem recebida Mam Caps \n\n".
-				  "Input_2431: $input_2431 \nInput_2985: $input_2985 \nInput_606: $input_606 \nOptin: $optin \n";
+				  "Tentando_Engravidar: $tentando_engravidar \n: $ \n: $ \nOptin_18394: $optin_18394 \n";
 	$headers = "MIME-Version: 1.0\r\nContent-type: text/plain; charset=UTF-8\r\n";	
 	$headers .= "From: send@naturfive.com\n";
 	$headers .= "Reply-To: DoNotReply@yoursite.com";	
