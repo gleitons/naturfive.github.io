40888e93c35363203825f3b1a9300549ca32eb1d
gleitons <31367377+gleitons@users.noreply.github.com>
2022-03-02 13:56:10 -0300

    blur

--- load-background.js
+++ load-background.js
@@ -1,3 +1,22 @@
+localStorage.setItem('b1_naturfive', `https://rogresph.sirv.com/Naturfive/Slides/essencialv1.jpeg`)
+
+localStorage.setItem('b2_naturfive', `https://rogresph.sirv.com/Naturfive/Slides/catv1.jpeg`)
+
+localStorage.setItem('b3_naturfive', `https://rogresph.sirv.com/Naturfive/Slides/essencial-refil.jpeg`)
+
+localStorage.setItem('b4_naturfive', `https://static.natura.com/cdn/ff/nxUPcPSteuv63ixr9ervJy971FZb6hP0Pbt0XtWKjSs/1644595263/public/styles/banner_full_desktop/public/2022-02/1920x695-Home-Desk_0.jpg?itok=UqtnN75-`)
+
+localStorage.setItem('b5_naturfive', `https://static.natura.com/cdn/ff/5QkWGr5ZwzFF73Uq2NPTm06UM0FyLXvBAFCOxCJwWiQ/1643060748/public/styles/banner_full_desktop/public/2022-01/kaiak-pulso-desk_0.jpg?itok=MYm2n4LG`)
+
+localStorage.setItem('b6_naturfive', `https://static.natura.com/cdn/ff/1IDiS2AGlPR0VyJegNy-T4O6DK013pl3lxX7SrBG_48/1644437488/public/styles/banner_full_desktop/public/2022-02/tododia-tamara-canela-desk.jpg?itok=QYi_i-j9`)
+
+localStorage.setItem('b7_naturfive', `https://static.natura.com/cdn/ff/ypktm3YBLg25TfEGrg3wxzpTW5YfmOU3fQgpf2FB-RA/1645638930/public/styles/banner_full_desktop/public/2022-02/1920x695%20-%20Tododia%20limao%20e%20hortel%C3%A3%20Casting.jpg?itok=_B3v-AQJ`)
+
+localStorage.setItem('b8_naturfive', `https://static.natura.com/cdn/ff/GF8LSsRe0t3knKT4R0W00lJ8MYlmMGZHB8arCii6uV8/1645638931/public/styles/banner_full_desktop/public/2022-02/1920x695.jpg?itok=tcADSKgL`)
+
+localStorage.setItem('logo_naturfive', `https://www.naturfive.com/img/logo-naturfive-naturfive.png`) 
+
+/*
 localStorage.setItem('b1_naturfive', `https://static.natura.com/cdn/ff/fii265E3jvD-Oh0TYE-3Hk_D-e2TmNsbvif3Qf6dwes/1644524379/public/styles/banner_full_desktop/public/2022-02/1920x695_v1_essencial.jpg?itok=ZkvD2JId`)
 
 localStorage.setItem('b2_naturfive', `https://static.natura.com/cdn/ff/tAWFyt8HxtZjLQUf8EtW9CswTLmyiBDtKrS0nEA63kM/1644938071/public/styles/banner_full_desktop/public/2022-02/1920x500-cat_0.jpg?itok=Vy4ZIS5B`)
@@ -15,3 +34,5 @@ localStorage.setItem('b7_naturfive', `https://static.natura.com/cdn/ff/ypktm3YBL
 localStorage.setItem('b8_naturfive', `https://static.natura.com/cdn/ff/GF8LSsRe0t3knKT4R0W00lJ8MYlmMGZHB8arCii6uV8/1645638931/public/styles/banner_full_desktop/public/2022-02/1920x695.jpg?itok=tcADSKgL`)
 
 localStorage.setItem('logo_naturfive', `https://www.naturfive.com/img/logo-naturfive-naturfive.png`) 
+
+*/
